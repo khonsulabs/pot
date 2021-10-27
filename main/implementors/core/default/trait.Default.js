7d3db4ee714d99f80c58e2b2476dc800121961dd
kl-botsu <botsu@khonsulabs.com>
2021-10-27 16:54:09 +0000

    Deploying to gh-pages from @ khonsulabs/pot@95a4ab5360040a511834e8b5b8d1a615acf49bbb 🚀

--- main/implementors/core/default/trait.Default.js
+++ main/implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pot"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"pot/ser/struct.SymbolMap.html\" title=\"struct pot::ser::SymbolMap\">SymbolMap</a>","synthetic":false,"types":["pot::ser::SymbolMap"]}];
+implementors["pot"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"pot/ser/struct.SymbolMap.html\" title=\"struct pot::ser::SymbolMap\">SymbolMap</a>","synthetic":false,"types":["pot::ser::SymbolMap"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- main/implementors/std/io/trait.Read.js
+++ main/implementors/std/io/trait.Read.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pot"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"pot/reader/struct.SliceReader.html\" title=\"struct pot::reader::SliceReader\">SliceReader</a>&lt;'a&gt;","synthetic":false,"types":["pot::reader::SliceReader"]}];
+implementors["pot"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"pot/reader/struct.SliceReader.html\" title=\"struct pot::reader::SliceReader\">SliceReader</a>&lt;'a&gt;","synthetic":false,"types":["pot::reader::SliceReader"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
