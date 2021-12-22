6b70ff854067e263bd733f370ba0ebc10c896b33
kl-botsu <botsu@khonsulabs.com>
2021-12-22 16:22:59 +0000

    Deploying to gh-pages from @ khonsulabs/pot@8c2af790a317c9cdbda5f83d42334a93f5a0364b 🚀

--- main/implementors/serde/de/trait.Error.js
+++ main/implementors/serde/de/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pot"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.131/serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"pot/enum.Error.html\" title=\"enum pot::Error\">Error</a>","synthetic":false,"types":["pot::error::Error"]}];
+implementors["pot"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.132/serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"pot/enum.Error.html\" title=\"enum pot::Error\">Error</a>","synthetic":false,"types":["pot::error::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
