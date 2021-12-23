7e30dbee30e2bd4900062a83d6ac3eb9a399c768
kl-botsu <botsu@khonsulabs.com>
2021-12-23 17:35:03 +0000

    Deploying to gh-pages from @ khonsulabs/pot@a2998ad8410e86b1e50ab16ab36a78881b1bb60f 🚀

--- main/source-files.js
+++ main/source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["pot"] = {"name":"","files":["de.rs","error.rs","format.rs","lib.rs","reader.rs","ser.rs"]};
+sourcesIndex["pot"] = {"name":"","files":["de.rs","error.rs","format.rs","lib.rs","reader.rs","ser.rs","value.rs"]};
 sourcesIndex["xtask"] = {"name":"","files":["main.rs"]};
 createSourceSidebar();
