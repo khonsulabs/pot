4275fa8c2df193a26d9202d751fe9d6642ebe7aa
Jonathan Johnson <jon@khonsulabs.com>
2021-12-30 07:36:00 -0800

    Updated khonsu-tools + rustme

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,16 +1,10 @@
 use khonsu_tools::{
-    anyhow,
-    code_coverage::{self, CodeCoverage},
+    universal::{anyhow, DefaultConfig},
     Commands,
 };
 use structopt::StructOpt;
 
 fn main() -> anyhow::Result<()> {
     let command = Commands::from_args();
-    match command {
-        Commands::GenerateCodeCoverageReport {
-            install_dependencies,
-        } => CodeCoverage::<code_coverage::DefaultConfig>::execute(install_dependencies),
-        Commands::InstallPreCommitHook | Commands::Audit { .. } => todo!(),
-    }
+    command.execute::<DefaultConfig>()
 }
