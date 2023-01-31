8fdebd42e402b96fccb6e17fda1fa5e8bba505f6
kl-botsu <botsu@khonsulabs.com>
2023-01-31 19:35:58 +0000

    Deploying to gh-pages from @ khonsulabs/pot@7fcbe19e7b8b8dfd49303d3b533f2f1d504eb775 🚀

---
+++ main/implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"pot":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"pot/struct.OwnedValue.html\" title=\"struct pot::OwnedValue\">OwnedValue</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
