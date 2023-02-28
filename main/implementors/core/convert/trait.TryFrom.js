6c9d9523e78e13150bcb6dc6c539a670e21a670e
kl-botsu <botsu@khonsulabs.com>
2023-02-28 16:36:47 +0000

    Deploying to gh-pages from @ khonsulabs/pot@3d136325fefb72a1589cfeb3c66bad2f67119f28 🚀

--- main/implementors/core/convert/trait.TryFrom.js
+++ main/implementors/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"pot":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.0/std/primitive.u64.html\">u64</a>&gt; for <a class=\"enum\" href=\"pot/format/enum.Special.html\" title=\"enum pot::format::Special\">Special</a>"]]
+"pot":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.1/std/primitive.u64.html\">u64</a>&gt; for <a class=\"enum\" href=\"pot/format/enum.Special.html\" title=\"enum pot::format::Special\">Special</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
