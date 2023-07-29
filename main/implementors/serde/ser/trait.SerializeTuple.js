aa54a2b43cac7dd3ceb6cd036c2398f21cf87e5e
kl-botsu <botsu@khonsulabs.com>
2023-07-29 18:53:40 +0000

    Deploying to gh-pages from @ khonsulabs/pot@dadcb1d782953831bde9408a807964d1a5fc845a 🚀

--- main/implementors/serde/ser/trait.SerializeTuple.js
+++ main/implementors/serde/ser/trait.SerializeTuple.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"pot":[["impl&lt;'de, 'a: 'de, W: WriteBytesExt + 'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.177/serde/ser/trait.SerializeTuple.html\" title=\"trait serde::ser::SerializeTuple\">SerializeTuple</a> for &amp;'de mut <a class=\"struct\" href=\"pot/ser/struct.Serializer.html\" title=\"struct pot::ser::Serializer\">Serializer</a>&lt;'a, W&gt;"]]
+"pot":[["impl&lt;'de, 'a: 'de, W: WriteBytesExt + 'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.178/serde/ser/trait.SerializeTuple.html\" title=\"trait serde::ser::SerializeTuple\">SerializeTuple</a> for &amp;'de mut <a class=\"struct\" href=\"pot/ser/struct.Serializer.html\" title=\"struct pot::ser::Serializer\">Serializer</a>&lt;'a, W&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
