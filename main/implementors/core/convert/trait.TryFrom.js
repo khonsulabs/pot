834811d311180dd4995c61a05138fb93a2c1c45e
kl-botsu <botsu@khonsulabs.com>
2023-08-24 14:23:05 +0000

    Deploying to gh-pages from @ khonsulabs/pot@a05e0007eaa39e126f814d9e47071c8177c328e5 🚀

--- main/implementors/core/convert/trait.TryFrom.js
+++ main/implementors/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"pot":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.71.0/std/primitive.u64.html\">u64</a>&gt; for <a class=\"enum\" href=\"pot/format/enum.Special.html\" title=\"enum pot::format::Special\">Special</a>"]]
+"pot":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.72.0/std/primitive.u64.html\">u64</a>&gt; for <a class=\"enum\" href=\"pot/format/enum.Special.html\" title=\"enum pot::format::Special\">Special</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- main/implementors/serde/ser/trait.SerializeTupleStruct.js
+++ main/implementors/serde/ser/trait.SerializeTupleStruct.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"pot":[["impl&lt;'de, 'a: 'de, W: WriteBytesExt + 'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.180/serde/ser/trait.SerializeTupleStruct.html\" title=\"trait serde::ser::SerializeTupleStruct\">SerializeTupleStruct</a> for &amp;'de mut <a class=\"struct\" href=\"pot/ser/struct.Serializer.html\" title=\"struct pot::ser::Serializer\">Serializer</a>&lt;'a, W&gt;"]]
+"pot":[["impl&lt;'de, 'a: 'de, W: WriteBytesExt + 'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.186/serde/ser/trait.SerializeTupleStruct.html\" title=\"trait serde::ser::SerializeTupleStruct\">SerializeTupleStruct</a> for &amp;'de mut <a class=\"struct\" href=\"pot/ser/struct.Serializer.html\" title=\"struct pot::ser::Serializer\">Serializer</a>&lt;'a, W&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
